2f946d6d128588beaacab10781fbe14fb62b685b
dayamoy <dayamoy@uthaan.co.in>
2021-06-11 08:49:08 +0530

    Added profiles #2

--- src/Components/Card/CircularProfileImage.js
+++ src/Components/Card/CircularProfileImage.js
@@ -2,12 +2,10 @@ import React from "react";
 
 export default function CircularProfileImage({ picture }) {
   return (
-
-      <img
-        className="rounded-full h-64 w-64 object-cover"
-        src={picture}
-        alt="Person image"
-      />
-
+    <img
+      className="rounded-full h-64 w-64 object-cover transform skew-y-6"
+      src={picture}
+      alt="Person image"
+    />
   );
 }
--- src/routes/Profiles.js
+++ src/routes/Profiles.js
@@ -9,16 +9,23 @@ export default function Profiles() {
       <Header
         gradient={true}
         headerText="Experts"
-        subHeaderText="Meet our experts"
+        subHeaderText="Meet our team of experts"
       />
       {expertList.map((expert) => (
-        <div className="md: flex flex-col md:flex-row p-16 content-center justify-center ">
+        <div className="md:flex flex-col md:flex-row p-16 content-center justify-center pb-24 mt-24 sm:my-0 sm:p-32 bg-green-500 transform -skew-y-6 ">
           <CircularProfileImage
+            key={expert.name}
             className="flex-auto"
             picture={expert.picture}
           />
-          <section className="flex-auto text-center text-justify self-center p-5 md:p-16">
-            <h1 className="text-center">{expert.name}</h1>
+          <section
+            key={expert.qualifications}
+            className="text-white pt-24 flex-auto text-center text-justify self-center p-5 md:px-16 transform skew-y-6"
+          >
+            <h1 className="text-center text-2xl font-bold">{expert.name}</h1>
+            <h6 className="text-center py-5 md:p-5 font-thin text-grey-500">
+              {expert.qualifications}
+            </h6>
             <p>{expert.description}</p>
           </section>
         </div>
